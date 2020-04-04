0499136dfbf3715b3d5db3fc276c057336464f5a
Crystal Broach <crysland@gmail.com>
2020-04-04 10:36:12 -0500

    Add project files.

---
+++ PickleAndHope/Controllers/PicklesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PickleAndHope.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicklesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddPickle(Pickle pickleToAdd)
+        {
+
+        }
+    }
+}
